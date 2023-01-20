c903ffcade0558f082187aace986a81701bbbf34
majulha <majulialcarvalho@gmail.com>
2023-01-20 10:41:59 -0300

    first commit

---
+++ ListOfGames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListOfGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
