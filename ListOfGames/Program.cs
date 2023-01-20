372f5c14b0565ab808a99198d36c1b041cd3b571
majulha <majulialcarvalho@gmail.com>
2023-01-20 18:38:34 -0300

    everything done

--- ListOfGames/Program.cs
+++ ListOfGames/Program.cs
@@ -23,9 +23,8 @@ namespace ListOfGames
             informations[2] = ReadLine();
             Client client = new Client(informations[0], DateTime.Parse(informations[1]), informations[2] );
             
-            WriteLine($"Nice to meet you, {informations[0]!} Now I need you to type how many games do you want to register :) ");
+            WriteLine($"Nice to meet you {informations[0]}! Now I need you to type how many games do you want to register :) ");
             informations[3] = ReadLine();
-            Cls();
             WriteLine($"\nOkay, it will be {informations[3]} games! ");
             WriteLine($"***********************************************************************************");
             Cls();
@@ -34,8 +33,9 @@ namespace ListOfGames
             //setting informations to pu at invoices list:
             WriteLine($"***********************************************************");
             WriteLine($"Now, I need to know the informations to make your list :)"  );
+            WriteLine($"***********************************************************");
             
-            for (int i = 0; i < int.Parse(informations[3]); i++)
+            for (int i = 1; i <= int.Parse(informations[3]); i++)
             {
                 WriteLine($"{i}º game!");
                 WriteLine("Enter the games title, date of purchase, company, status of release and the value");
@@ -57,20 +57,21 @@ namespace ListOfGames
             WriteLine(" Yay! We did it! Tell me a month/year you want to check how much did you spent      ");
 
             informations[9] = ("YES");
-            while (informations[9].ToUpper() == "YES")
+            do
             {
                 informations[10] = ReadLine();
                 informations[11] = ReadLine();
 
                 WriteLine($"At month {informations[10]}/{informations[11]}, you spend " +
-                          $"{client.MonthValue(int.Parse(informations[10]),int.Parse(informations[11])).ToString($"F2", CultureInfo.InvariantCulture)}");
-                
+                          $"{client.MonthValue(int.Parse(informations[10]), int.Parse(informations[11])).ToString($"F2", CultureInfo.InvariantCulture)}");
+
                 WriteLine($"\nTotally, you spend {client.TotalValue().ToString($"F2", CultureInfo.InvariantCulture)}");
-                
                 Cls();
+                
                 WriteLine("Do you want check another month? [YES / NO]");
                 informations[9] = ReadLine();
-            }
+            } while (informations[9].ToUpper() == "YES");
+            
             WriteLine("************************************************************************************");
             WriteLine($"It was fun to made your calculations! Have a nice day ( ﾉ ﾟｰﾟ)ﾉ");
             WriteLine("************************************************************************************");
