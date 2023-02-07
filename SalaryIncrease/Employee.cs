82942b67155d8788132015494f3f9c4ea89daa01
majulha <majulialcarvalho@gmail.com>
2023-02-07 19:08:53 -0300

    problem solved

---
+++ SalaryIncrease/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace SalaryIncrease
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public double Salary { get; set; }
+        
+        public Employee(){}
+
+        public Employee(string name, string id, double salary)
+        {
+            Name = name;
+            ID = id;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double porcentage)
+        {
+            Salary = (Salary * porcentage) + Salary;
+        }
+        
+        public override string ToString()
+        {
+            return ($"ID: {ID}, Name: {Name}, Salary: U${Salary} ");
+        }
+    }
+}
--- SalaryIncrease/Program.cs
+++ SalaryIncrease/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using static System.Console;
 
 namespace SalaryIncrease
@@ -7,7 +9,51 @@ namespace SalaryIncrease
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            WriteLine($"Salary Increase: Welcome Carls!");
+            WriteLine($"-------------------------\n");
+            
+            WriteLine($"Please, enter with a number of employees");
+            short numberOfEmployees = short.Parse(ReadLine());
+            WriteLine($"-------------------------\n");
+            
+            WriteLine($"Nice! Will be {numberOfEmployees} employees :)");
+            WriteLine($"Now, you will type the ID, Name and Salary from each employee. Id doesnt repeat. ");
+
+            List<Employee> DataEmployees = new List<Employee>();
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                WriteLine($"{i+1} employee: ");
+                WriteLine($"Enter with an Id");
+                string id = ReadLine();
+
+                foreach (Employee person in DataEmployees)
+                {
+                    if (id == person.ID)
+                    {
+                        do
+                        {
+                            WriteLine($"Ids cant be the same! Enter with a valid one");
+                            id = ReadLine();
+
+                        } while (person.ID == id);
+                    }
+                }
+                WriteLine($"Enter with a name: ");
+                string name = ReadLine();
+                WriteLine($"Enter with the salary");
+                double salary = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+
+                Employee employee = new Employee(name, id, salary);
+                DataEmployees.Add(employee);
+            }
+
+            WriteLine($"Nice! Now Ill list everyone:");
+            foreach (Employee person in DataEmployees)
+            {
+                WriteLine($"{person}\n");
+            }
+            
         }
     }
 }
